f3e4ede7dd001ed35843d801a26d6a5d05bc436e
YeonIsFree <68737910+YeonIsFree@users.noreply.github.com>
2022-10-07 10:13:57 +0900

    Oct.7.2022
    10952번 문제 코드 누락 수정

--- 단계별 문제풀이/반복문/10952.swift
+++ 단계별 문제풀이/반복문/10952.swift
@@ -1,1 +1,17 @@
+import Foundation
+
+/*
+    < 10952: A+B - 5 >  작성: YeonIsFree
+    
+ */
+
+// [우선 While문으로 무조건 진입]
+while true {
+    let input: [Int] = readLine()!.split(separator: " ").map{(value) -> Int in return Int(value)!}
+// [ 입력값으로 "0 0"이 들어오면 break로 While 루프를 탈출한다 ]
+    if input[0] == 0 && input[1] == 0 {
+        break
+    }
+    print(input[0]+input[1])
+}
 
