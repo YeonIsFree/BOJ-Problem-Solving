861694d52d8b555fd8deb5939b4aa177d7e5a715
YeonIsFree <68737910+YeonIsFree@users.noreply.github.com>
2022-10-09 23:53:54 +0900

    Oct.9.2022

--- 단계별 문제풀이/1차원 배열/2562.swift
+++ 단계별 문제풀이/1차원 배열/2562.swift
@@ -2,11 +2,12 @@ import Foundation
 
 /*
     < 2562: 최댓값 >  작성: YeonIsFree
+    강의에서 분명 firstIndex를 들은 것 같은데 가물가물했다.
  */
 
 var arr: [Int] = []
 var maxNum = 0
-
+// [입력값을 arr에 하나씩 붙이고, maxNum을 업데이트 해준다]
 for i in 0...8{
     arr.append(Int(readLine()!)!)
     if arr[i] > maxNum {
@@ -15,4 +16,5 @@ for i in 0...8{
 }
 
 print(maxNum)
+// [firstIndex는 of:에 입력된 값이 나오는 첫번째 인덱스를 의미한다. 배열에 중복된 값이 있을 경우를 고려한 듯]
 print(arr.firstIndex(of: maxNum)! + 1)
