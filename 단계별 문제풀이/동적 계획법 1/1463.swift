71dd6b8af6301971d962ccf801594e9c43ae1ac6
YeonIsFree <seryun91@naver.com>
2022-11-20 18:45:28 +0900

    Nov. 20. 2022

---
+++ 단계별 문제풀이/동적 계획법 1/1463.swift
@@ -0,0 +1,26 @@
+
+/*
+ < 1463: 1로 만들기 >  작성: YeonIsFree
+ */
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+var dp: [Int] = Array(repeating: 0, count: n + 1)
+
+if n == 1 {
+    print(dp[n])
+}else {
+    for i in 2...n {
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0 {
+            dp[i] = min(dp[i], dp[i / 2] + 1)
+        }
+        if i % 3 == 0 {
+            dp[i] = min(dp[i], dp[i / 3] + 1)
+        }
+    }
+    print(dp[n])
+}
+
