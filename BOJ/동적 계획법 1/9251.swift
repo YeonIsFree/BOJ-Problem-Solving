2d1c41339ee87b23f2f837bc3ee5dc6a3eb25294
YeonIsFree <seryun91@naver.com>
2022-12-02 08:34:57 +0900

    Dec. 2. 2022
    9251. LCS

---
+++ BOJ/동적 계획법 1/9251.swift
@@ -0,0 +1,25 @@
+
+/*
+ < 9251. LCS  >  작성: YeonIsFree
+ LCS는 Longest Common Sequeces의 약자로 대표적인 DP문제이다.
+ 예전에 풀었던 최대 부분 배열 길이 (Longest Common "Subarray") 문제와는 다르게 "Sequece"는 문자열이 연속되지 않아도 된다.
+ */
+
+import Foundation
+
+let m = Array(readLine()!)
+let n = Array(readLine()!)
+var dp = Array(repeating: Array(repeating: 0, count: n.count + 1), count: m.count + 1)
+
+for i in 1...m.count {
+    for j in 1...n.count {
+        if m[i-1] == n[j-1] {
+            dp[i][j] = dp[i-1][j-1] + 1
+        } else {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        }
+    }
+}
+
+print(dp[m.count][n.count])
+
