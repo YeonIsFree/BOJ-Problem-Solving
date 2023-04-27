27d154708039b45e158db0601cbb8b61e5f7fa5a
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-04-27 12:32:54 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int ans;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    
+    while(n--) {
+        ans++;
+        cout << ans << '\n';
+    }
+}
