029fbb7d92297657e93955e64c7ce91555987db3
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-04-27 13:45:36 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i = n; i > 0; i--) { 
+        cout << n;
+        cout << '\n';
+        n--;
+    }
+}
