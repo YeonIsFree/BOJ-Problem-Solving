b98cf60ba16c65aaaba1b7ed15585a387bd1ea8e
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-04-28 16:57:48 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int input[9][9];
-pair<int, int> a;
+int num;
+int a, b; // 최대 좌표를 저장할 변수
+int mx; // 최대값
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
-            cin >> input[i][j];
-        }
-    }
-
-    int mx = -1;
-    int prev_mx = mx;
-    
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            // 현재 만난 수와 mx에 저장되어있는 수를 비교하여 mx를 갱신한다.
-            mx = max(prev_mx, input[i][j]);
-            // mx가 갱신되었다면 좌표를 저장한다.
-            if(prev_mx != mx)
-                a = {i+1, j+1};
-            // 현재 mx를 잠시 저장해둔다.
-            prev_mx = mx;
+            cin >> num;
+            if(num > mx) {
+                mx = num;
+                a = i;
+                b = j;
+            }
         }
     }
     cout << mx << '\n';
-    cout << a.first << ' ' << a.second;
+    cout << a+1 << ' ' << b+1;
+    return 0;
 }
