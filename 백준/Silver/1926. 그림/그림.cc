8bde043165469af61976caa0c2ca49c48b0f1364
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-04-29 17:00:24 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3256 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -3,37 +3,32 @@ using namespace std;
 #define X first
 #define Y second
 
-int board[503][503];
-bool vis[503][503];
 int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+int board[503][503];
+bool vis[503][503];
+
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> n >> m;
+   
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> board[i][j];
         }
     }
-
-    int num = 0;
+    queue<pair<int, int> > Q;
+    int cnt = 0;
     int mx = 0;
-    // 이중 for 문을 돌면서 bfs의 시작점이 될 수 있는 지점을 찾는다.
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            // 이때 이미 방문한 칸이나 그림이 없는 칸은 그냥 지나친다.
-            if(vis[i][j] || board[i][j] == 0) continue;
-            // 여기에 진입 했다는 것은 새 그림을 발견했다는 뜻
-            num++; 
-            
-            // 이제 bfs를 돌린다.
-            queue<pair<int, int> > Q;
-            vis[i][j] = 1;
+            if(board[i][j] == 0 || vis[i][j] == true) continue;
             Q.push({i, j});
+            vis[i][j] = true;
+            cnt++;
 
             int area = 0;
             while(!Q.empty()) {
@@ -42,14 +37,15 @@ int main(void) {
                 for(int dir = 0; dir < 4; dir++) {
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if(vis[nx][ny] || board[nx][ny] == 0) continue;
-                    vis[nx][ny] = 1;
+                    if(nx < 0 || nx > n || ny < 0 || ny > m) continue;
+                    if(vis[nx][ny] || board[nx][ny] == 0) continue; 
                     Q.push({nx, ny});
+                    vis[nx][ny] = true;
                 }
             }
             mx = max(mx, area);
         }
     }
-    cout << num << '\n' << mx;
+    cout << cnt << '\n' << mx;
+
 }
