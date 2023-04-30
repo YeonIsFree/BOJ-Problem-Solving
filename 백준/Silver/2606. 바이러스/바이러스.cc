1e7d42fbdabb4e08087cad958b413c1e9e41182d
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-01 08:54:17 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -3,8 +3,7 @@ using namespace std;
 
 int v, e;
 int cnt;
-vector<int> adj[103];
-// { [1| 2, 5],  [2| ],  [3| ],  [] ...}
+vector<int> pc[103];
 bool vis[103];
 
 void bfs() {
@@ -14,28 +13,23 @@ void bfs() {
     while(!Q.empty()) {
         int cur = Q.front(); Q.pop();
         cnt++;
-        for(auto nx : adj[cur]) {
-            // 이미 감염된 pc면 패스
+        for(int nx : pc[cur]) {
             if(vis[nx]) continue;
             Q.push(nx);
             vis[nx] = true;
         }
     }
 }
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> v >> e;
     for(int i = 0; i < e; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        int a, b = 0;
+        cin >> a >> b;
+        pc[a].push_back(b);
+        pc[b].push_back(a);
     }
-
     bfs();
-    // 1번 컴퓨터 카운트는 하나 빼주기
     cout << cnt - 1;
-
 }
