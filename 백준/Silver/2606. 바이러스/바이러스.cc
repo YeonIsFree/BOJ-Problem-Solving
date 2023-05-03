0ba900fe671479548f4860a3ecdda1cfcced3881
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-03 10:51:32 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,35 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int v, e;
-int cnt;
-vector<int> pc[103];
+vector<int> arr[103];
 bool vis[103];
+int v, e, cnt;
 
-void bfs() {
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> v;
+    cin >> e;
+    for(int i = 0; i < e; i++) {
+        int a, b = 0;
+        cin >> a >> b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+
     queue<int> Q;
     Q.push(1);
     vis[1] = true;
     while(!Q.empty()) {
         int cur = Q.front(); Q.pop();
         cnt++;
-        for(int nx : pc[cur]) {
+        for(int nx : arr[cur]) {
             if(vis[nx]) continue;
-            Q.push(nx);
             vis[nx] = true;
+            Q.push(nx); 
         }
     }
-}
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> v >> e;
-    for(int i = 0; i < e; i++) {
-        int a, b = 0;
-        cin >> a >> b;
-        pc[a].push_back(b);
-        pc[b].push_back(a);
-    }
-    bfs();
+
     cout << cnt - 1;
 }
