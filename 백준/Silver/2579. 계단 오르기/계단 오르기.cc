46aad8f46f7d014cc3544f281edd2d7e7862d8e2
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-09 08:39:48 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,30 +2,20 @@
 using namespace std;
 
 int n;
-int dp[303][3];
 int score[303];
-
-// dp[i][j] : 현재까지 j개의 계단을 연속으로 밟고 i로 올라옴
+int dp[303];
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
-    for(int i = 1; i <= n; i++)
+    for(int i = 1; i <=n; i++)
         cin >> score[i];
-    // 초기값 설정
-    dp[1][1] = score[1], dp[1][2] = 0;
-    dp[2][1] = score[2], dp[2][2] = score[1] + score[2];
+    
+    dp[1] = score[1], dp[2] = score[1] + score[2];
+    for(int i = 3; i <= n; i++) 
+        dp[i] = max(dp[i-2], dp[i-3] + score[i-1]) + score[i];
+    
+    cout << dp[n];
 
-    if(n == 1) {
-        cout << score[1];
-        return 0;
-    }
-
-    for(int i = 3; i <= n; i++) {
-        dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + score[i];
-        dp[i][2] = dp[i-1][1] + score[i];
-    }
-
-    cout << max(dp[n][1], dp[n][2]);
 }
