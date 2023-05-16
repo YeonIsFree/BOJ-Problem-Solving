7ef5fa784e8e9376e58e3a2d735a491e4ecb1cc6
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-16 14:29:35 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int m, n, cnt;
+int m, n;
+vector<bool> isprime(1000002, true);
 
-bool isprime(int n) {
-    if(n == 1) return 0;
-    for(int i = 2; i * i <= n; i++)
-        if(n % i == 0) return 0;
-    return 1;
+void func(int n) {
+   isprime[1] = false;
+   for(int i = 2; i*i <= n; i++) {
+    if(isprime[i] == false) continue;
+    for(int j = i*i; j <= n; j += i) {
+        isprime[j] = false;
+    } 
+   }
 }
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> m >> n;
-    for(int i = m; i <= n; i++)
-        if(isprime(i)) cout << i << '\n';
+    func(n);
+    for(int i = m; i <= n; i++) {
+        if(isprime[i]) cout << i << '\n';
+    }
+
 }
