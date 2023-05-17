bb3b4c858a5a24b832d5b56ecd3be971eb8b341e
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-17 11:35:21 +0900

    [Silver IV] Title: 수 찾기, Time: 60 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int st, en, mid;
+int arr1[100003];
+int arr2[100003];
+
+int bst(int i) {
+    // 초기 인덱스
+    st = 0;
+    en = n-1;
+    mid = (st + en) / 2;
+    
+    int target = i;
+
+    while(st != mid && en != mid) {
+        mid = (st + en) / 2;
+
+        // target이 mid와 같다면
+        if(target == arr1[mid])
+            return 1;
+
+        // target이 mid보다 크다면 (오른쪽에 있음)
+        else if(target > arr1[mid])
+            st = mid + 1;
+
+        // target이 mid보다 작다면 (왼쪽)
+        else if(target < arr1[mid])
+            en = mid - 1;   
+
+    } // while문 종료까지 이 함수가 종료되지 않았다는 것은 target이 없다는 뜻
+    return 0;
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr1[i];
+    sort(arr1, arr1 + n);
+
+    cin >> m;
+    for(int i = 0; i < m; i++)
+        cin >> arr2[i];
+
+    for(int i = 0; i < m; i++)
+        cout << bst(arr2[i]) << '\n';
+    
+}
