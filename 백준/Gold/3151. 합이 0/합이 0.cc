7020e2a7ecb0d7ac8177dbcfb5c9b0b0098079b4
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-19 13:41:45 +0900

    [Gold IV] Title: 합이 0, Time: 1984 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+long long cnt;
+int arr[10003];
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for (int i = 0; i < n; i++) 
+    cin >> arr[i];
+
+  sort(arr, arr + n);
+
+  for(int i = 0; i < n - 1; i++) {
+    for(int j = i + 1; j < n; j++) {
+        int target = -(arr[i] + arr[j]);
+        cnt += upper_bound(arr + j + 1, arr + n, target) - lower_bound(arr + j + 1, arr + n, target);
+    }
+  }
+  cout << cnt;
+}
