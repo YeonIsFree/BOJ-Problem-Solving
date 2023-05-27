2d672ee5e6a2264744fc95b8358e65563e5df47d
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-05-27 16:19:54 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -5,42 +5,42 @@ int n, m;
 vector<pair<int, int> > adj[1003];
 bool vis[1003];
 
-// okay 간선이 2개일때 오류가 난다!!!!! 합을 매번 pair로 가져가야겠다. 
-
-void func(int st, int en) {
+void bfs(int st, int en) {
+    fill(vis, vis+n+1, 0);
     queue<pair<int, int> > Q;
     Q.push({st, 0});
     vis[st] = true;
     while(!Q.empty()) {
         auto [cur, dist] = Q.front(); Q.pop();
-        if(cur == en) {
-            cout << dist << '\n';
-            return;
-        }
+
         for(auto [nx, nx_dist] : adj[cur]) {
             if(vis[nx]) continue;
             Q.push({nx, dist + nx_dist});
             vis[nx] = true;
+
+            if(nx == en) {
+                cout << dist + nx_dist;
+                return;
+            }
         }
     }
 }
-
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    for(int i = 1; i <= n-1; i++) {
-        int a = 0, b = 0, c = 0;
+    for(int i = 0; i < n-1; i++) {
+        int a, b, c;
         cin >> a >> b >> c;
         adj[a].push_back({b, c});
         adj[b].push_back({a, c});
     }
 
     while(m--) {
-        fill(vis, vis+n+1, false);
         int a, b;
         cin >> a >> b;
-        func(a, b);
+        bfs(a, b);
+        cout << '\n';
     }
 }
