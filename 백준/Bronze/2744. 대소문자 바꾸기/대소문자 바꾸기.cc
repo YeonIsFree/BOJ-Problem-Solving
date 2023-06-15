b2e4c78fe3a9f6e901340fb1cd081beb2cc97e75
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-06-15 17:46:03 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+string str_rev;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> str;
+    for(char c : str) {
+        // 대문자 -> 소문자
+        if(c <= 'Z') cout << char(c + 32);
+
+        // 소문자 -> 대문자
+        else if(c >= 'a') cout << char(c - 32);
+    }
+}
