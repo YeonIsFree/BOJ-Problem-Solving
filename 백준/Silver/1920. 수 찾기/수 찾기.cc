f9d0d1e6165b9331a167ff8d57fc6de892748fad
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-06-16 15:00:41 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,33 +2,8 @@
 using namespace std;
 
 int n, m;
-int st, en, mid;
-int arr1[100003];
-int arr2[100003];
-
-int bst(int target) {
-    // 초기 인덱스
-    st = 0;
-    en = n-1;
-
-    while(st <= en) {
-        mid = (st + en) / 2;
-
-        // target이 mid와 같다면 (찾았다!)
-        if(target == arr1[mid])
-            return 1;
-
-        // target이 mid보다 크다면 (오른쪽에 있음)
-        else if(target > arr1[mid])
-            st = mid + 1;
-
-        // target이 mid보다 작다면 (왼쪽)
-        else if(target < arr1[mid])
-            en = mid - 1;   
-
-    } // while문 종료까지 이 함수가 종료되지 않았다는 것은 target이 없다는 뜻
-    return 0;
-}
+int arr1[100001];
+int arr2[100001];
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -36,13 +11,15 @@ int main(void) {
     cin >> n;
     for(int i = 0; i < n; i++)
         cin >> arr1[i];
-    sort(arr1, arr1 + n);
-
     cin >> m;
     for(int i = 0; i < m; i++)
         cin >> arr2[i];
 
-    for(int i = 0; i < m; i++)
-        cout << bst(arr2[i]) << '\n';
-    
+    sort(arr1, arr1+n);
+
+    for(int i = 0; i < m; i++) {
+        int target = arr2[i];
+        if(binary_search(arr1, arr1+n, target)) cout << 1 << '\n';
+        else cout << 0 << '\n';
+    }
 }
