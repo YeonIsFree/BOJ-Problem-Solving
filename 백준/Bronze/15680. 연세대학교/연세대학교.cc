bf5ef5edaac0368a4d36fa96ae3bc789668156f9
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-06-22 13:39:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    if(n == 0) cout << "YONSEI" << '\n';
+    else cout << "Leading the Way to the Future" <<'\n';
+
+}
