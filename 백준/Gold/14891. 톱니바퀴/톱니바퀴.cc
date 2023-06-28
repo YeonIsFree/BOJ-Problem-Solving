2b7d273bd0cc120973f2977a975080e3e1f79f8f
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-06-29 08:14:57 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -1,67 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> gears;
-string s;
-int k, num;
+vector<string> gears(4);
+vector<int> dirs(4);
+int k;
+int num, d;
 
-vector<int> dir_check(int cur, int dir) {
-    vector<int> dirs(4);
-    dirs[cur] = dir;
+void run() {
+    for(int i = 0; i < 4; i++) {
+        // 반시계 방향 회전
+        if(dirs[i] == -1) rotate(gears[i].begin(), gears[i].begin()+1, gears[i].end());
+        // 시계 방향 회전
+        else if(dirs[i] == 1) rotate(gears[i].begin(), gears[i].begin()+7, gears[i].end());
+    }
+}
 
-    // cur 기준 왼쪽 진행
-    int idx = cur;
+void dir_check(int num) {
+    // num 번 기어를 중심으로 왼쪽 진행
+    int idx = num;
     while(idx > 0 && gears[idx][6] != gears[idx - 1][2]) {
         dirs[idx - 1] = -dirs[idx];
         idx--;
     }
 
-    // cur 기준 오른쪽 진행
-    idx = cur;
+    // num 번 기어를 중심으로 오른쪽 진행
+    idx = num;
     while(idx < 3 && gears[idx][2] != gears[idx + 1][6]) {
         dirs[idx + 1] = -dirs[idx];
         idx++;
-    }
-
-    return dirs;
-}
-
-void run(vector<int> dirs) { 
-    for(int i = 0; i < 4; i++) {
-        // 해당 기어의 회전 방향이 -1 이면 반시계 방향 회전
-        if(dirs[i] == -1) rotate(gears[i].begin(), gears[i].begin()+1, gears[i].end());
-        // 해당 기어의 회전 방향이 1 이면 시계 방향회전
-        else if(dirs[i] == 1) rotate(gears[i].begin(), gears[i].begin()+7, gears[i].end());
     }
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    for(int i = 0; i < 4; i++) {
-        cin >> s;
-        gears.push_back(s);
-    }
+    for(int i = 0; i < 4; i++) cin >> gears[i];
     cin >> k;
     while(k--) {
-        int num_tmp, dir;
-        cin >> num_tmp;
-        num = num_tmp - 1;
-        cin >> dir;
+        int tmp;
+        cin >> tmp >> d;
 
-        // 1) num 번 기어를 시작으로 각 기어들의 회전 방향을 결정한다
-        auto v = dir_check(num, dir);
+        num = tmp - 1;
         
-        // 2) 각 기어들을 위에서 결정된 방향으로 돌려준다. - rotate() 이용
-        run(v);
-    }
+        fill(dirs.begin(), dirs.end(), 0);
 
-    // 3) 점수를 계산한다.
+        // 1) 먼저 num 번 톱니 바퀴를 중심으로 각 톱니 바퀴들의 회전 방향을 결정한다.
+        dirs[num] = d;
+        dir_check(num);
+
+        // 2) 위에서 결정된 방향으로 각 톱니 바퀴들을 돌린다.
+        run();
+    }
+        // 3) 점수를 계산한다.
     int score = 0;
     for(int i = 0; i < 4; i++)
         if(gears[i][0] == '1') score += (1 << i);
-        
-    // 4) 출력
+    
     cout << score;
-
+    
 }
