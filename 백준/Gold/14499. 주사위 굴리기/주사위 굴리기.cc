6672c35551863b886cda14553fa148ef40458e44
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-05 08:30:26 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dx[4] = {0, 0, -1, 1};  // 동 0, 서 1, 북 2, 남 3
+int dy[4] = {1, -1, 0, 0};
+
+int BOTTOM = 0, TOP = 1, FRONT = 2, REAR = 3, LEFT = 4, RIGHT = 5;
+
 int n, m, x, y, k;
 int board[22][22];
 int dice[6];
-
-int TOP = 0, BOTTOM = 1, FRONT = 2, REAR = 3, LEFT = 4, RIGHT = 5;
-
-int dx[4] = {0, 0, -1, 1}; // 동 서 북 남 순서로.
-int dy[4] = {1, -1, 0, 0};
-
-bool isOOB(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < m;
-}
 
 void rotate(int a, int b, int c, int d) {
     int tmp = dice[d];
@@ -22,23 +18,21 @@ void rotate(int a, int b, int c, int d) {
     dice[a] = tmp;
 }
 
-// 주사위를 해당 방향으로 굴리고 TOP의 값을 반환하는 함수
 int roll(int dir, int x, int y) {
-    // 주어진 방향에 따라 주사위를 굴려준다.
-    if(dir == 0) // 동쪽
+    // 각 방향에 따른 회전
+    if(dir == 0) // 동
         rotate(BOTTOM, LEFT, TOP, RIGHT);
-    else if(dir == 1)// 서쪽
+    else if(dir == 1)  // 서
         rotate(BOTTOM, RIGHT, TOP, LEFT);
-    else if(dir == 2)// 북쪽
+    else if(dir == 2)  // 북
         rotate(BOTTOM, FRONT, TOP, REAR);
-    else // 남쪽
+    else if(dir == 3)  // 남
         rotate(BOTTOM, REAR, TOP, FRONT);
-    
-    // 바닥면의 값에 따라,
-    //  1) 해당 칸의 값이 0 일 경우, 주사위의 값 -> 보드로
-    if(board[x][y] == 0)
-        board[x][y] = dice[BOTTOM];
-    //  2) 해당 칸의 값이 0 이 아닌 경우, 보드의 값 -> 주사위로, 그 뒤 보드는 0
+
+    // 지도에 써있는 숫자가
+    //  1) 0 이면 주사위 -> 지도
+    if(board[x][y] == 0) board[x][y] = dice[BOTTOM];
+    //  2) 0 이 아니면 지도 -> 주사위, 지도는 0 이 됨
     else {
         dice[BOTTOM] = board[x][y];
         board[x][y] = 0;
@@ -56,8 +50,8 @@ int main(void) {
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
             cin >> board[i][j];
-    
-    // 각 명령어에 맞춰 주사위를 굴려준다.
+
+    // ---------
     while(k--) {
         int dir = 0;
         cin >> dir;
@@ -66,15 +60,12 @@ int main(void) {
         int nx = x + dx[dir];
         int ny = y + dy[dir];
 
-        // board 범위 안에 들어오는지 검사
-        if(!isOOB(nx, ny)) continue;
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 
-        // 주사위를 해당 방향에 맞게 굴리고 TOP을 출력 한다.
         int top = roll(dir, nx, ny);
         cout << top << '\n';
 
-        // 다음 턴을 위해 현재 위치 갱신
         x = nx;
         y = ny;
-    }    
+    }
 }
