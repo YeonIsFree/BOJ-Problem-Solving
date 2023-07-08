fec23059203752846840f1c6c3a73bce265405dc
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-08 20:08:25 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};  // 동 0, 서 1, 북 2, 남 3
-int dy[4] = {1, -1, 0, 0};
+int TOP = 0, BOTTOM = 1, FRONT = 2, REAR = 3, LEFT = 4, RIGHT = 5;
 
-int BOTTOM = 0, TOP = 1, FRONT = 2, REAR = 3, LEFT = 4, RIGHT = 5;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
 
 int n, m, x, y, k;
 int board[22][22];
@@ -18,29 +18,28 @@ void rotate(int a, int b, int c, int d) {
     dice[a] = tmp;
 }
 
-int roll(int dir, int x, int y) {
-    // 각 방향에 따른 회전
+void roll(int dir, int x, int y) {
+    // 주사위를 굴린다.
     if(dir == 0) // 동
         rotate(BOTTOM, LEFT, TOP, RIGHT);
-    else if(dir == 1)  // 서
+    else if(dir == 1) // 서
         rotate(BOTTOM, RIGHT, TOP, LEFT);
-    else if(dir == 2)  // 북
+    else if(dir == 2) // 북
         rotate(BOTTOM, FRONT, TOP, REAR);
-    else if(dir == 3)  // 남
+    else if(dir == 3)// 남
         rotate(BOTTOM, REAR, TOP, FRONT);
 
-    // 지도에 써있는 숫자가
-    //  1) 0 이면 주사위 -> 지도
-    if(board[x][y] == 0) board[x][y] = dice[BOTTOM];
-    //  2) 0 이 아니면 지도 -> 주사위, 지도는 0 이 됨
+    // 보드의 상태에 따른 액션
+    if(board[x][y] == 0) {
+        board[x][y] = dice[BOTTOM];
+    }
     else {
         dice[BOTTOM] = board[x][y];
         board[x][y] = 0;
     }
 
-    return dice[TOP];
+    cout << dice[TOP] << '\n';
 }
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -51,8 +50,8 @@ int main(void) {
         for(int j = 0; j < m; j++)
             cin >> board[i][j];
 
-    // ---------
-    while(k--) {
+    // ---
+    for(int i = 0; i < k; i++) {
         int dir = 0;
         cin >> dir;
         dir -= 1;
@@ -60,10 +59,9 @@ int main(void) {
         int nx = x + dx[dir];
         int ny = y + dy[dir];
 
-        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
-        int top = roll(dir, nx, ny);
-        cout << top << '\n';
+        roll(dir, nx, ny);
 
         x = nx;
         y = ny;
