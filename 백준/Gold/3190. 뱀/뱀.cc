10ffbc20115afdc6720891b786c3bffae94c9be6
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-12 08:32:10 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,13 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-int n, k, l;
+int n, k, a, b, l, c;
+char d;
 int board[102][102];
-deque<pair<int, int>> snake;
 queue<pair<int, char>> orders;
+deque<pair<int, int>> Q;    // 뱀의 몸통
 int sec;
 
 int main(void) {
@@ -17,53 +20,51 @@ int main(void) {
     // 입력
     cin >> n >> k;
     for(int i = 0; i < k; i++) {
-        int cur_x = 0, cur_y = 0;
-        cin >> cur_x >> cur_y;
-        board[cur_x][cur_y] = -1;
+        cin >> a >> b;
+        board[a-1][b-1] = -1;
     }
     cin >> l;
     for(int i = 0; i < l; i++) {
-        int x;
-        char c;
-        cin >> x >> c;
-        orders.push({x, c});
+        cin >> c >> d;
+        orders.push({c, d});
     }
 
-    // ---
-    int dir = 1; // 초기 방향: 오른쪽
-    snake.push_back({1, 1});
-
+    // 초기 정보
+    Q.push_back({0, 0});
+    int dir = 1;
+    
+    // -----
     while(1) {
-        // 현재 좌표에 몸 표시
-        auto cur = snake.front();
-        board[cur.first][cur.second] = 1;
+        // 현재 좌표로 몸 표시
+        auto cur = Q.front();
+        board[cur.X][cur.Y] = 1;
 
-        // 다음 좌표 결정 + 시간 1초 흐름
-        int nx = cur.first + dx[dir];
-        int ny = cur.second + dy[dir];
-
+        // 다음 좌표 구하기
+        int nx = cur.X + dx[dir];
+        int ny = cur.Y + dy[dir];
+  
+        // 1초 경과
         sec++;
 
-        // 벽에 닿았거나 몸에 닿았으면 탈출
-        if(nx < 1 || ny < 1 || nx > n || ny > n) break;
+        // 만약 몸에 닿거나 벽에 닿으면 게임 오버
+        if(nx < 0 || ny < 0 || nx >= n || ny >= n) break;
         if(board[nx][ny] == 1) break;
 
-        // 현재 좌표로 머리 늘리기
-        snake.push_front({nx, ny});
+        // 이동한 칸으로 머리를 늘려준다.
+        Q.push_front({nx, ny});
 
-        // 만약 다음 좌표에 사과가 아니라면 꼬리를 당겨와서 길이를 유지한 채 전진
+        // 만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다.
         if(board[nx][ny] != -1) {
-            auto tail = snake.back();
-            board[tail.first][tail.second] = 0;
-            snake.pop_back();
+            auto tail = Q.back();
+            board[tail.X][tail.Y] = 0;
+            Q.pop_back();
         }
 
-        // 때가 되면 방향 회전
-        int order_time = orders.front().first;
-        if(!orders.empty() && sec == order_time) {
-            char order = orders.front().second;
-            if(order == 'D') dir = (dir + 1) % 4;
-            else if(order == 'L') dir = (dir + 3) % 4;
+        // 때가 되면 회전
+        auto order = orders.front();
+        if(order.X == sec) {
+            if(order.Y == 'L') dir = (dir+3) % 4;
+            else if(order.Y == 'D') dir = (dir+1) % 4;
             orders.pop();
         }
     }
