005907df237b9368d649d3f3e66fda7e13284031
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-12 14:30:10 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.16 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -3,23 +3,17 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
-    vector<int> tmp = arr;
     
-    // 입력으로 들어오는 배열 길이가 1일 경우 -1 리턴
+    // 입력 배열의 길이가 1인 경우 -1을 리턴
     if(arr.size() == 1) {
         answer.push_back(-1);
         return answer;
     }
     
-    // 최솟값 mn 찾기
-    sort(tmp.begin(), tmp.end());
-    int mn = tmp[0];
-    
-    // 최솟값 mn을 제외한 숫자를 차곡차곡 answer에 쌓는다.
-    for(int num : arr) {
-        if(num == mn) continue;
-        answer.push_back(num);
-    }
-    
+    // 최소값의 iterator 를 mn_it에 담고 erase 로 지워준다.
+    answer = arr;
+    auto mn_it = min_element(answer.begin(), answer.end());
+    answer.erase(mn_it);
     return answer;
 }
+
