334f89f9b5ccceaf850da50d611478802ce84ab2
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-14 10:49:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.49 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int zero_total;    // 제거한 모든 0의 개수
+int cnt;           // 변환 진행 횟수
+vector<int> ans;   
+
+vector<int> solution(string s) {
+    string target = s;
+   while(target != "1") {
+       // 변환 횟수를 올려준다.
+       cnt++;
+       
+       // 제거할 0의 개수 세어주기
+       int zero = 0;
+       string tmp = "";
+       for(char c : target) {
+           if(c == '0') {
+               zero++;
+               continue;
+           }
+           else tmp += c;
+       }
+       zero_total += zero;
+       
+       // 0 제거 후 길이를 이진 변환
+       int len = tmp.size();
+       target = "";
+       while(len > 0) {
+           target += to_string(len % 2);
+           len /= 2;
+       }
+       reverse(target.begin(), target.end());
+   }
+    
+    ans.push_back(cnt);
+    ans.push_back(zero_total);
+    
+    return ans;
+    
+}
