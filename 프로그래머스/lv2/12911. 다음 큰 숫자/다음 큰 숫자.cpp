566be22bebeca5ab99cdc472b7e2f120b85f0d1a
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-19 12:56:43 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,45 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tmp;
-int ans;
-
-string convert_to_binary(int num) {
-    string ret = "";
-    while(num > 0) {
-        ret += to_string(num % 2);
-        num /= 2;
-    }
-    return ret;
-}
-
 int solution(int n) {
-    int target = n;
+    int cnt = bitset<20> (n).count();
+    
     while(1) {
-        
-        // 현재 숫자를 string 타입 이진수로 변환한다.
-        string binary_n = convert_to_binary(target);
-        
-        // 1의 갯수를 세어준다.
-        int cnt = 0;
-        for(char c : binary_n)
-            if(c == '1') cnt++;
-        
-        // n의 1의 갯수 저장
-        if(target == n) {
-            tmp = cnt;
-            target++;
-            continue;
-        }
-        
-        // 1의 갯수가 같다면 종료
-        if(cnt == tmp) {
-            ans = target;
+        if(bitset<20> (++n).count() == cnt)
             break;
-        }
-        
-        target++;
     }
     
-    return ans;
+    return n;
 }
