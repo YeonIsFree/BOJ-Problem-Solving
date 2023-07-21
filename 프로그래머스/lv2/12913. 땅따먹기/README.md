55076b83ba550ce536432045571260ece71ca999
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-22 01:08:33 +0900

    [level 2] Title: 땅따먹기, Time: 6.26 ms, Memory: 27.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/README.md
+++ 프로그래머스/lv2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 27.3 MB, 시간: 6.60 ms
+메모리: 27.5 MB, 시간: 6.26 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 int dp[100002][4];
+int mx;
 
 int solution(vector<vector<int> > land)
 {
     int len = land.size();
     
-    // 초기값 세팅
     for(int i = 0; i < 4; i++) dp[0][i] = land[0][i];
     
-    // ---
-    for(int i = 1; i < land.size(); i++) {
-        dp[i][0] = max( {dp[i-1][1], dp[i-1][2], dp[i-1][3]} ) + land[i][0];
-        dp[i][1] = max( {dp[i-1][0], dp[i-1][2], dp[i-1][3]} ) + land[i][1];
-        dp[i][2] = max( {dp[i-1][0], dp[i-1][1], dp[i-1][3]} ) + land[i][2];
-        dp[i][3] = max( {dp[i-1][0], dp[i-1][1], dp[i-1][2]} ) + land[i][3];
+    for(int i = 1; i < len; i++) {
+        for(int j = 0; j < 4; j++) {
+            dp[i][0] = max({dp[i-1][1], dp[i-1][2], dp[i-1][3]}) + land[i][0];
+            dp[i][1] = max({dp[i-1][0], dp[i-1][2], dp[i-1][3]}) + land[i][1];
+            dp[i][2] = max({dp[i-1][0], dp[i-1][1], dp[i-1][3]}) + land[i][2];
+            dp[i][3] = max({dp[i-1][0], dp[i-1][1], dp[i-1][2]}) + land[i][3];
+        }
     }
     
-    int answer = 0;
-    for(int i = 0; i < 4; i++) 
-        answer = max(dp[len-1][i], answer);
+    for(int i = 0; i < 4; i++)
+        mx = max(dp[len-1][i], mx);
     
-    return answer;
+    return mx;
+    
 }
