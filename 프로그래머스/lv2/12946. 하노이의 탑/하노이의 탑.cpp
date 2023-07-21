62b774ea4e595225a5c44389142042772d855792
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2023-07-22 00:55:44 +0900

    [level 2] Title: 하노이의 탑, Time: 25.92 ms, Memory: 29.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int> > ans;
+vector<vector<int>> answer;
 
 void func(int a, int b, int n) {
-    if(n == 1) { 
+    if(n == 1) {
         vector<int> tmp = {a, b};
-        ans.push_back(tmp);
+        answer.push_back(tmp);
         return;
     }
-    // 1) a 기둥에 있는 n-1개의 원판을 6-a-b 기둥으로 옮겨준다.
+    
+    // n-1개의 원판을 6-a-b번 기둥으로 옮긴다.
     func(a, 6-a-b, n-1);
-    // 2) a 기둥에 있는 n번 원판을 b 기둥으로 옮겨준다.
+    // n번 원판을 목적지에 가져다 놓는다.
     vector<int> tmp = {a, b};
-    ans.push_back(tmp);
-    // 3) 6-a-b 기둥에 있는 n-1개의 원판을 b 기둥으로 옮겨준다.
+    answer.push_back(tmp);
+    // 6-a-b번 기둥에 있던 원판들을 목적지로 옮긴다.
     func(6-a-b, b, n-1);
-}
+} 
 
 vector<vector<int>> solution(int n) {
-    func(1, 3, n);
+
+    func(1, 3, n); 
     
-    return ans;
+    return answer;
 }
